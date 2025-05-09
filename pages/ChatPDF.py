b29ce1f090ac20420f7eb1dc00f6a484d65f4db5
kimdoochan2 <gimdoosol@gmail.com>
2025-05-09 23:00:54 +0900

    Update ChatPDF.py

--- pages/ChatPDF.py
+++ pages/ChatPDF.py
@@ -1,10 +1,10 @@
-import openai
-st.sidebar.write("📦 OpenAI 버전:", openai.__version__)
 
 import streamlit as st
 from openai import OpenAI
 import tempfile
 import time
+import openai
+st.sidebar.write("📦 OpenAI 버전:", openai.__version__)
 
 st.set_page_config(page_title="ChatPDF", layout="centered")
 
